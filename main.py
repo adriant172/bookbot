964175b4fa28530a62c904fe33d2824ebcc67e86
Matthew Thomas <adriant172@gmail.com>
2023-12-28 01:02:07 +0900

    add convert_to_list function and create logic to print report

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def main():
-    with open("books/frankenstein.txt", encoding="UTF-8") as f:
+    book = "books/frankenstein.txt"
+    with open(book, encoding="UTF-8") as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        print(f"Word count: {word_count}")
+        letter_count = count_letters(file_contents)
+        print(f"--- Beginning report of {book} ---")
+        print(f"{word_count} words were found in the document")
+        print(" ")
+        new_letter_list = convert_to_list(letter_count)
+        for letter in new_letter_list:
+            if letter[0].isalpha():
+                print(f"The \'{letter[0]}\' character was found {letter[1]} times")
+        print("--- End Report ---")
+        
 
 
 def count_words(book_text):
@@ -19,7 +29,15 @@ def count_letters(book_text):
             if letter in letter_count:
                 letter_count[letter] += 1
             else:
-                letter_count[letter] = 0
-    print(letter_count)
+                letter_count[letter] = 1
+    return letter_count
+
+def convert_to_list(letter_dict):
+    list_of_letters = []
+    for letter in letter_dict:
+        list_of_letters.append((letter ,letter_dict[letter]))
+    list_of_letters.sort()
+    return list_of_letters
+
 
 main()
